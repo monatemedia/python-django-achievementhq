c024102590b0245c776563bdd813e80594cec467
Edward Baitsewe <edward@monatemedia.com>
2024-08-06 09:28:24 +0200

    Updated all template/posts views

--- achievementhq/posts/forms.py
+++ achievementhq/posts/forms.py
@@ -5,3 +5,21 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['heading_text', 'message_text']
+    
+    heading_text = forms.CharField(
+        max_length=60,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter your title...',
+            'maxlength': '60'
+        }),
+        error_messages={'max_length': 'Title cannot exceed 60 characters.'}
+    )
+    
+    message_text = forms.CharField(
+        max_length=280,
+        widget=forms.Textarea(attrs={
+            'placeholder': 'Enter your message...',
+            'maxlength': '280'
+        }),
+        error_messages={'max_length': 'Message cannot exceed 280 characters.'}
+    )
--- achievementhq/posts/views.py
+++ achievementhq/posts/views.py
@@ -15,7 +15,6 @@ def index(request):
         'posts': posts,
         'user_joined_date': user_joined_date,
         'user_name': user_name,
-        'current_year': current_year
     })
 
 # Create a new post
@@ -31,7 +30,7 @@ def create(request):
             return redirect('posts:index')
     else:
         form = PostForm()
-    return render(request, 'posts/form.html', {'form': form, 'title': 'Create Post'})
+    return render(request, 'posts/form.html', {'form': form, 'title': 'Create Post', 'is_editing': False})
 
 # Read a specific post
 @login_required
@@ -50,7 +49,7 @@ def update(request, post_id):
             return redirect('posts:detail', post_id=post.id)
     else:
         form = PostForm(instance=post)
-    return render(request, 'posts/form.html', {'form': form, 'title': 'Edit Post'})
+    return render(request, 'posts/form.html', {'form': form, 'title': 'Edit Post', 'post': post, 'is_editing': True})
 
 # Delete a post
 @login_required
