b0952f46c18ed3441b19505057c8eadfffe76c8e
Edward Baitsewe <edward@monatemedia.com>
2024-08-15 09:17:32 +0200

    Add Posts and Comments functionality to 8000/admin area

--- achievementhq/posts/models.py
+++ achievementhq/posts/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     heading_text = models.CharField(max_length=60)
     message_text = models.CharField(max_length=280)
     pub_date = models.DateTimeField(default=datetime.now)
+    is_approved = models.BooleanField(default=True)
 
     def __str__(self):
         return self.heading_text
@@ -16,6 +17,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
     comments_text = models.CharField(max_length=280)
     pub_date = models.DateTimeField(default=datetime.now)
+    is_approved = models.BooleanField(default=True)
 
     def __str__(self):
         return self.comments_text
