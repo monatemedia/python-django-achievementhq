4f2e11582c7c33dd99875b35895096c5d13756db
Edward Baitsewe <edward@monatemedia.com>
2024-11-05 06:29:00 +0200

    Update settings.py

--- achievementhq/achievementhq/settings.py
+++ achievementhq/achievementhq/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DJANGO_SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY', default='my-op8_b#)na_0f!t88l(oxk+#5-f$k_bjadeqe$=zj0&zmy9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DJANGO_DEBUG', default=True, cast=bool)
