b7bdcb7f57bb28446bac6df103a06027ca37042b
Edward Baitsewe <edward@monatemedia.com>
2025-03-04 00:25:08 +0200

    Set up fake data factories for live server.

--- achievementhq/posts/management/commands/create_posts.py
+++ achievementhq/posts/management/commands/create_posts.py
@@ -18,9 +18,13 @@ class Command(BaseCommand):
                 pub_date = fake.date_time_between(start_date=user.date_joined, end_date='now')
                 # Ensure pub_date is timezone-aware
                 pub_date = timezone.make_aware(pub_date)
+                
+                # Ensure heading_text does not exceed the maximum length of 60 characters
+                heading_text = fake.sentence(nb_words=6)[:60]
+                
                 Post.objects.create(
                     user=user,
-                    heading_text=fake.sentence(nb_words=6),
+                    heading_text=heading_text,
                     message_text=fake.text(max_nb_chars=280),
                     pub_date=pub_date
                 )
--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -2,5 +2,9 @@
 
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-python production-demo.py --noinput
+python manage.py create_superuser_if_not_exists
+python manage.py create_users
+python manage.py create_posts
+python manage.py create_comments
+python manage.py populate_questions
 python -m gunicorn --bind 0.0.0.0:8000 --workers 3 achievementhq.wsgi:application
